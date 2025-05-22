fd4467080966fb5c072de0bc260ce41b473b03e2
Berna Kurt <beekurt98@gmail.com>
2025-05-22 22:01:13 +0300

    feat: integrate ProfileLinks component and fetch profile data
    Add ProfileLinks component to the live page and fetch profile data from the API using the provided slug. This change enhances the page by displaying relevant profile links dynamically.

--- src/app/(live)/[slug]/page.jsx
+++ src/app/(live)/[slug]/page.jsx
@@ -1,5 +1,17 @@
+import ProfileLinks from "@/components/ProfileLinks";
+
 export default async function Page({ params }) {
   const { slug } = await params;
+  const url = `${
+    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+  }/api/profile/${slug}`;
 
-  return <>{slug}</>;
+  const { data } = await fetch(url).then((r) => r.json());
+
+  return (
+    <>
+      <ProfileLinks data={data} />
+      <div className="purple-bg"></div>
+    </>
+  );
 }
